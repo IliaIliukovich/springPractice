9d4b55f80432bbfc0312eb94118a7bbad9cb393e
Roman Dogoda <piterom911@gmail.com>
2025-03-06 14:49:45 +0100

    DB H2 TransactionRepository
    implementation

--- src/main/java/com/telran/springpractice/entity/Transaction.java
+++ src/main/java/com/telran/springpractice/entity/Transaction.java
@@ -2,6 +2,10 @@ package com.telran.springpractice.entity;
 
 import com.telran.springpractice.entity.enums.TransactionStatus;
 import com.telran.springpractice.entity.enums.TransactionType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -11,16 +15,20 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
 public class Transaction {
 
+    @Id
     private String id;
 
+    @Enumerated(EnumType.STRING)
     private TransactionType type;
 
     private BigDecimal amount;
 
     private String description;
 
+    @Enumerated(EnumType.STRING)
     private TransactionStatus status;
 
     private Long fromAccountId;// получатель
---
+++ src/main/java/com/telran/springpractice/service/TransactionService.java
@@ -0,0 +1,37 @@
+package com.telran.springpractice.service;
+
+import com.telran.springpractice.entity.Transaction;
+import com.telran.springpractice.entity.enums.TransactionType;
+import com.telran.springpractice.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Service
+public class TransactionService {
+
+    private final TransactionRepository repository;
+
+    @Autowired
+    public TransactionService(TransactionRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Transaction> getAll() {
+        return repository.findAll();
+    }
+
+    public Transaction create(Transaction transaction) {
+        return repository.save(transaction);
+    }
+
+    public List<Transaction> searchByType(TransactionType type, BigDecimal minAmount) {
+        List<Transaction> all = repository.findAll();
+        Predicate<Transaction> predicate = t -> (type == null || (type == t.getType())) &&
+                (minAmount == null || t.getAmount().compareTo(minAmount) > 0);
+        return all.stream().filter(predicate).toList();
+    }
+}
